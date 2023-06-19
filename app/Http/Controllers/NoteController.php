af039503a82521d2fb0f0abe6b9d6021d3816f12
Boukiour <mboukiour@mborijnland.nl>
2023-06-19 14:16:41 +0200

    app toegevoegd

---
+++ app/Http/Controllers/NoteController.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Note;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class NoteController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $notes = Note::where('user_id', Auth::user()->id)->latest('updated_at')->paginate(5);
+        return view('notes.index')->with('notes', $notes);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('notes.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => 'required|max:120',
+            'text' => 'required'
+        ]);
+
+        $note = new Note([              //nieuw object aan maken van Note model
+            'user_id' => Auth::id(),    //id van de huidige user in user_id zetten
+            'title' => $request->title, //title (request) in title zetten
+            'text' => $request->text    //text (request) in text zetten
+        ]);
+        $note->save();                  //het object opslaan en dus de rij opslaan in de tabel
+
+        return to_route('notes.index'); //redirect naar de route notes.index
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Note  $note
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Note $note)
+    {
+        return view('notes.show')->with('note', $note);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Note  $note
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Note $note)
+    {
+        return view('notes.edit')->with('note', $note);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Note  $note
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Note $note)
+    {
+        $request->validate([
+            'title' => 'required|max:120',
+            'text' => 'required'
+        ]);
+
+        $note->update([
+            'title' => $request->title,
+            'text' => $request->text
+        ]);
+
+        return to_route('notes.show', $note);
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Note  $note
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Note $note)
+    {
+        $note->delete();
+        return to_route('notes.index');
+
+    }
+}
---
+++ resources/views/notes/index.blade.php
@@ -0,0 +1,30 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Notes') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <a href="{{ route('notes.create') }}" class="btn-link btn-lg mb-2">+ New Note</a>
+            @foreach ($notes as $note)
+                <div class="my-6 p-6 bg-white border-b border-gray-200 shadow-sm sm:rounded-lg">
+
+                    <h2 class="font-bold text-2xl">
+                        <a href="{{ route('notes.show', $note) }}">{{ $note->title }}</a>
+                    </h2>
+
+                        <p>
+                            {{ Str::limit( $note->text, 200 ) }}
+                        </p>
+                        <span>
+                        {{ $note->updated_at->diffForHumans()  }}
+                    </span>
+
+                </div>
+            @endforeach
+                {{ $notes->links() }}
+        </div>
+    </div>
+</x-app-layout>
